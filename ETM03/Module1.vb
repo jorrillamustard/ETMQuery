fa0528fe3adfbc24ea5c526424bf29e320935aa1
jorrillamustard <merrick.kyle@gmail.com>
2016-07-22 08:35:35 -0500

    fixes

--- ETM03/Module1.vb
+++ ETM03/Module1.vb
@@ -61,7 +61,7 @@ Module Module1
 
             End If
 
-                If My.Application.CommandLineArgs(3) = True Then
+            If My.Application.CommandLineArgs(3) = True Then
                 Debug.WriteLine("Getting Network Events...")
                 If My.Application.CommandLineArgs(10) = True Then
 
@@ -95,7 +95,7 @@ Module Module1
 
             End If
 
-                If My.Application.CommandLineArgs(5) = True Then
+            If My.Application.CommandLineArgs(5) = True Then
                 Debug.WriteLine("Getting Image Events...")
                 If My.Application.CommandLineArgs(10) = True Then
                     Dim ImageThread As New EventThread
